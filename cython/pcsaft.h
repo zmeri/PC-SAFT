43b7e4c7937e9ac6b62e3c57fca4b4208d30d851
Zach Baird <ztbaird@gmail.com>
2018-12-27 14:12:54 +0200

    Added bubbleT function

--- cython/pcsaft.h
+++ cython/pcsaft.h
@@ -43,6 +43,8 @@ double pcsaft_gres_cpp(vector<double> x, vector<double> m, vector<double> s, vec
 
 double bubblePfit_cpp(double p_guess, vector<double> xv_guess, vector<double> x, vector<double> m, vector<double> s, vector<double> e,
     double t, add_args &cppargs);
+double bubbleTfit_cpp(double t_guess, vector<double> xv_guess, vector<double> x, vector<double> m, vector<double> s, vector<double> e,
+    double t, add_args &cppargs);
 double PTzfit_cpp(double p_guess, vector<double> x_guess, double beta_guess, double mol, 
     double vol, vector<double> x_total, vector<double> m, vector<double> s, vector<double> e,
     double t, add_args &cppargs);
