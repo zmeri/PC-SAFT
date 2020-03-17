4bd03f8720fa2f29a5240a9800bb5d33ada9094f
Zach Baird <ztbaird@gmail.com>
2020-03-17 22:03:13 +0200

    Added info to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup file for compiling to Cython module
 
 @author: Zach Baird
 """
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy as np
 
@@ -13,6 +13,22 @@ ext_modules = [
         sources=["pcsaft_electrolyte.pyx"],
         language="c++")]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pcsaft',
-      version='1.0',
+      version='1.0.0',
+      author="Zach Baird",
+      description="The PC-SAFT equation of state, including dipole, association and ion terms.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/zmeri/PC-SAFT",
+      packages=find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: C++",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+      ],
       ext_modules=cythonize(ext_modules, language_level="3"))
