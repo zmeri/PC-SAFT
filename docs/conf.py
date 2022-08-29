0664b838e6784e6d6032e16b5619c047e16fd00e
zmeri <22378936+zmeri@users.noreply.github.com>
2022-08-29 22:54:32 +0300

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Zach Baird'
 author = 'Zach Baird'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.0'
+release = '1.4.1'
 
 
 # -- General configuration ---------------------------------------------------
